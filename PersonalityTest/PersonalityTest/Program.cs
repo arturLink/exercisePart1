cb4763fb8078e91a29c18e789efa94981ad520bc
opilane <opilane@HP-CZC0359NNX>
2021-12-07 09:54:08 +0200

    did stuff

---
+++ PersonalityTest/PersonalityTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the program asks user to enter their fav colour
+            //displays personality traits
+            //whatever they are
+            //if user enters anything else program displays "you are (userInput) unicorn"
+            
+            Console.WriteLine("Enter your favorite colour: ");
+            string colour = Console.ReadLine();
+            if (colour == "Red")
+                Console.WriteLine($"You picked {colour}. You are romantic.");
+            else if (colour == "Blue")
+                Console.WriteLine($"You picked {colour}. You are reliable.");
+            else if (colour == "Yellow")
+                Console.WriteLine($"You picked {colour}. You are cheerful.");
+            else
+                Console.WriteLine($"You picked {colour}. You are unicorn.");
+        }
+    }
+}
